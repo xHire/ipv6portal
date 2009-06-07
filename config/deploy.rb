6df974d481674793f2db493993980f626de1f112
xHire <xhire@tuxportal.cz>
2009-06-08 00:02:47 +0200

    Set up deploying via capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "ipv6portal"
+set :repository,  "xhire@localhost:/srv/git/ipv6portal.git"
+set :scm, :git
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+role :web, "sykora.paskuli.cz"
+role :app, "sykora.paskuli.cz"
+role :db,  "sykora.paskuli.cz", :primary => true
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :deploy_to, "/srv/web/ipv6portal.cz/www/"
+set :user, "xhire"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :use_sudo, false
+
+task :after_update_code, :roles => [:app, :db] do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
+
+
+namespace :deploy do
+	task :start, :roles => :app do
+	end
+end
+
+namespace :deploy do
+	desc "Restart Application"
+	task :restart, :roles => :app do
+		run "touch #{current_path}/tmp/restart.txt"
+	end
+end
