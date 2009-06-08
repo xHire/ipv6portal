5aa7b01feb8ee7bd04bf268dd3d8565ec4ee3185
xHire <xhire@tuxportal.cz>
2009-06-08 15:43:51 +0200

    Generated empty controller Database::Providers

---
+++ app/controllers/database/providers_controller.rb
@@ -0,0 +1,3 @@
+# coding: utf-8
+class Database::ProvidersController < Database::BaseController
+end
---
+++ test/unit/helpers/database/providers_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Database::ProvidersHelperTest < ActionView::TestCase
+end
