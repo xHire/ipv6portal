671a600aa23e64df8d9fa8f908a8212f3aa01156
xHire <xhire@tuxportal.cz>
2009-06-08 20:06:15 +0200

    Renaming one variable in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 	map.namespace :database, :name_prefix => 'db_', :path_prefix => 'databaze' do |db|
-		db.providers			':provider_type', :controller => 'providers', :action => 'list'
-		db.provider_show	':provider_type/:id/detail', :controller => 'providers', :action => 'show'
+		db.providers			':type', :controller => 'providers', :action => 'list'
+		db.provider_show	':type/:id/detail', :controller => 'providers', :action => 'show'
 	end
 
 	map.db 'databaze', :controller => 'database/base'
