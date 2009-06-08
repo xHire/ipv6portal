4d6c0eb81a25c206ba7608db48d8a9a3bef50d10
xHire <xhire@tuxportal.cz>
2009-06-08 23:34:29 +0200

    Basic listing of providers

--- app/controllers/database/providers_controller.rb
+++ app/controllers/database/providers_controller.rb
@@ -2,6 +2,26 @@
 class Database::ProvidersController < Database::BaseController
 	before_filter :select_category
 
+	def list
+		# title
+		@title = 'Databáze '
+		case params['type']
+			when 'isp'
+				@title += 'ISP'
+			when 'serverhousing'
+				@title += 'serverhousingů'
+			when 'webhosting'
+				@title += 'webhostingů'
+			else
+				@title = 'Chyba'
+				@providers = nil
+				return
+		end
+
+		# data for listing
+		@providers = Object.module_eval("::#{params['type'].camelize}", __FILE__, __LINE__).all
+	end
+
 	private
 	def select_category
 		@selected_category = params['type']
--- app/helpers/database/providers_helper.rb
+++ app/helpers/database/providers_helper.rb
@@ -1,2 +1,14 @@
 module Database::ProvidersHelper
+	def status_string status
+		case status
+			when 1
+				'podporuje'
+			when 2
+				'v plánu'
+			when 3
+				'bez zájmu'
+			else
+				'???'
+		end
+	end
 end
