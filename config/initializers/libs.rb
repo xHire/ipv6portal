8649b8213fa11de4fd04ab46929c36696d5641f7
xHire <xhire@tuxportal.cz>
2009-06-15 23:19:01 +0200

    Extension of AR - adding a column after another

--- config/initializers/libs.rb
+++ config/initializers/libs.rb
@@ -1,1 +1,2 @@
 require File.join(RAILS_ROOT, 'lib', 'string')
+require File.join(RAILS_ROOT, 'lib', 'activerecord')
---
+++ lib/activerecord.rb
@@ -0,0 +1,12 @@
+module ActiveRecord
+	module ConnectionAdapters # :nodoc:
+		module SchemaStatements
+			def add_column(table_name, column_name, type, options = {})
+				add_column_sql = "ALTER TABLE #{quote_table_name(table_name)} ADD #{quote_column_name(column_name)} #{type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
+				add_column_options!(add_column_sql, options)
+				add_column_sql += " AFTER #{quote_column_name(options[:after])}" if options[:after]
+				execute(add_column_sql)
+			end
+		end
+	end
+end
