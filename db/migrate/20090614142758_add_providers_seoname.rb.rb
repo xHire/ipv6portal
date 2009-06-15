9b60c74c3cf0a67340dcf123b5b9375d83e36224
xHire <xhire@tuxportal.cz>
2009-06-15 23:30:06 +0200

    When adding crucial column, migrate data into it

--- db/migrate/20090614142758_add_providers_seoname.rb.rb
+++ db/migrate/20090614142758_add_providers_seoname.rb.rb
@@ -1,6 +1,12 @@
 class AddProvidersSeoname < ActiveRecord::Migration
 	def self.up
 		add_column :providers, :seoname, :string, :limit => 35, :null => false, :after => :name
+
+		providers = Provider.all
+		for provider in providers do
+			provider.seoname = provider.name.seolize
+			provider.save!
+		end
 	end
 
 	def self.down
